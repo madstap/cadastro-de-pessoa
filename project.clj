a9d381f5d1da19301520ac5aae95e145c1111bff
Aleksander Madland Stapnes <aleksander_m_stapnes@hotmail.com>
2016-05-23 17:56:24 -0300

    bump version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject cadastro-de-pessoa "0.1.0-SNAPSHOT"
+(defproject cadastro-de-pessoa "0.1.0"
   :description "Some utils for working with cpfs and cnpjs."
   :url "http://github.com/madstap/cadastro-de-pessoa"
   :license {:name "Eclipse Public License"
